ea3d311489422cc22e8181e91da80a81159bbd95
Anthony Vadala <git@anthonyvadala.me>
2022-04-27 09:21:19 -0400

    Update install.sh
    Updated some comments and script headers

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Define install directories and names
 install_path="$HOME/.pokemonsay"
@@ -6,19 +6,19 @@ bin_path="$HOME/bin"
 pokemonsay_bin="pokemonsay"
 pokemonthink_bin="pokemonthink"
 
-# Make sure the directories exist
+# Make sure directories exist
 mkdir -p $install_path/
 mkdir -p $install_path/cows/
 mkdir -p $bin_path/
 
-# Copy the cows and the main script to the install path.
+# Copy the cows and the main script to the install path
 cp ./cows/*.cow $install_path/cows/
 cp ./pokemonsay.sh $install_path/
 cp ./pokemonthink.sh $install_path/
 
-# Create the pokemonsay script in the home bin directory.
+# Create the pokemonsay script in the home bin directory
 cat > $bin_path/$pokemonsay_bin <<- EOF
-	#!/bin/sh
+	#!/usr/bin/env bash
 
 	# This script changes to the pokemonsay installation directory,
 	# runs the main script for running the pokemonsay, and changes
@@ -29,9 +29,9 @@ cat > $bin_path/$pokemonsay_bin <<- EOF
 	cd - >/dev/null
 EOF
 
-# Create the pokemonthink script in the home bin directory.
+# Create the pokemonthink script in the home bin directory
 cat > $bin_path/$pokemonthink_bin <<- EOF
-	#!/bin/sh
+	#!/usr/bin/env bash
 
 	# This script changes to the pokemonsay installation directory,
 	# runs the main script for running the pokemonthink, and changes
@@ -44,7 +44,7 @@ EOF
 
 # Create uninstall script in the install directory
 cat > $install_path/uninstall.sh <<- EOF
-	#!/bin/sh
+	#!/usr/bin/env bash
 
 	#
 	# This script uninstalls pokemonsay.
