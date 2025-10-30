b7d2deb1fadceadf4977491f241a834253b56dde
Anthony Vadala <git@anthonyvadala.me>
2025-10-30 17:50:27 -0400

    Updated make_cows.sh script

--- build/make_cows.sh
+++ build/make_cows.sh
@@ -5,12 +5,10 @@ if [ ! -d "cows" ]; then
 	mkdir cows
 fi
 
-for fullfilename in images/*.png
+for fullfilename in ./scrapped-data/*.png
 do
 	filename=$(basename "$fullfilename")
 	extension="${filename##*.}"
 	filename="${filename%.*}"
-	#convert "$fullfilename" -trim +repage "$fullfilename.trim"
-	#img2xterm --cow "$fullfilename.trim" "cows/$filename.cow"
 	img2xterm --cow "$fullfilename" "cows/$filename.cow"
 done
